6a6e2dfbffae045dfbb88ed32540af092074cb4f
Jonathan Shavit <jshavit30@stuy.edu>
2021-12-21 11:03:54 -0500

    started StringToInt. need to finish and then add a case for negatives

---
+++ 12.21.21/StringToInt.java
@@ -0,0 +1,8 @@
+public class StringToInt{
+  public static void main(String[] args){
+    System.out.println(stringToInt(args[0]) == Integer.parseInt(args[0]));
+  }
+  public static int stringToInt(String str){
+    return 0;
+  }
+}
