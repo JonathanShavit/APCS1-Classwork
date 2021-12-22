597f45d84331a42d2db160e36ecbe356859fe9fa
Jonathan Shavit <jshavit30@stuy.edu>
2021-12-21 23:21:25 -0500

    updated method to work properly with string representations of negative ints

--- 12.21.21/StringToInt.java
+++ 12.21.21/StringToInt.java
@@ -3,14 +3,22 @@ public class StringToInt{
     System.out.println(stringToInt(args[0]) == Integer.parseInt(args[0]));
   }
   public static int stringToInt(String str){
+    int modifier;
+    if (str.charAt(0) == '-'){
+      str = str.substring(1);
+      modifier = -1;
+    } else{
+      modifier = 1;
+    }
     int ans = 0;
     for (int i = 0; i < str.length(); i++){
       ans+=valueOfDigit(str.charAt(i));
       if (i != str.length() - 1){
-        ans*=10;
+        ans *= 10;
       }
     }
-    //System.out.prinln(ans);
+    ans*=modifier;
+    //System.out.println(ans);
     return ans;
   }
   public static int valueOfDigit(char c){
